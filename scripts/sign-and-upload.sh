8855c52c1528c69f43dbcd7968905a6bcfaf7b79
Pawan Ahire <ahire.pawan@gmail.com>
2018-05-25 13:19:33 +0530

    Update sign-and-upload.sh

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -15,3 +15,11 @@ xcodebuild \
 -configuration Release \
 PROVISIONING_PROFILE="$PROVISIONING_PROFILE" \
 archive 
+
+# Export the archive to an ipa
+xcodebuild \
+-exportArchive \
+-archivePath "$OUTPUTDIR/$APP_NAME.xcarchive" \
+-exportPath "$OUTPUTDIR"
+
+#-exportOptionsPlist "${EXPORT_PLIST}" \
